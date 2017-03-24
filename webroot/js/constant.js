8a60d2437721e717666253e92bc3fe2bb293eb5d
Paulo <paulobiscocho>
2017-03-24 16:53:10 +0800

    HOSTING BASE URL ON CONSTANT JS

--- webroot/js/constant.js
+++ webroot/js/constant.js
@@ -1,5 +1,5 @@
 if (document.location.hostname == "localhost" || document.location.hostname == "192.168.1.10"){
 	var BASE_URL = '/balloon-nation/';
 }else{
-	var	BASE_URL = '';
+	var	BASE_URL = 'http://balloonrep.tssi-erb.com/';
 }
