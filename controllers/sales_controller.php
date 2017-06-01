279271ce231104979834804203318e5f796987a8
Paulo <paulobiscocho>
2017-06-01 12:16:15 +0800

    Cotumer Order BY

--- controllers/sales_controller.php
+++ controllers/sales_controller.php
@@ -75,6 +75,7 @@ class SalesController extends AppController {
 
 	function admin_add() {
 		if (!empty($this->data)) {
+			//pr($this->data);exit;
 			
 			$this->Sale->deleteAll(array(
 				'Sale.customer_id' => $this->data['Sale']['customer_id'], 
@@ -159,7 +160,7 @@ class SalesController extends AppController {
 	function initial_data(){
 		$data = array();
 		$this->Customer->unbindModel( array('hasMany' => array('Product')));
-		$data['Customers'] = $this->Customer->find('all',array('order' =>array('Customer.modified DESC')));
+		$data['Customers'] = $this->Customer->find('all',array('order' =>array('Customer.modified')));
 		$data['InclusiveDates'] = $this->InclusiveDate->find('all',array('conditions'=>array('InclusiveDate.group'=>'semi-monthly')));
 		echo json_encode($data);
 		exit;
@@ -170,25 +171,50 @@ class SalesController extends AppController {
 		$customer_id = $this->data['customer_id'];
 		$from_date = $this->data['from'];
 		$to_date = $this->data['to'];
-	
 		$data =array();
-		$data['Result'] = $this->Sale->find('first',array(
+		
+		//GET SALE (USE FOR CHECKING SALE STATUS)
+		$sale = $this->Sale->find('first',array(
 								'recursive'=>2,
 								'conditions'=> array(
 									'Sale.customer_id' => $customer_id,
 									'Sale.from_date >=' => $from_date,
 									'Sale.to_date <=' => $to_date,
-							)
-					));
+							)));
+		//CHECK IF POSTED
+		if(empty($sale) || $sale['Sale']['is_posted'] !=  1){
 			
-		if(empty($data['Result']) || $data['Result']['Sale']['is_posted'] != 1){
-			$data['Result'] = $this->Sale->get_data($customer_id,$from_date,$to_date);
-			$data['is_posted'] = false;
+				$data['is_posted'] =  false;
+				//GET SALE DETAILS 
+				$saleDetails = $this->Sale->get_data($customer_id,$from_date,$to_date);
+				
+				//GET PRODUCTS
+				$this->Product->unbindModel( array('hasMany' => array('ProductImage','Deliviries','DeliveryDetail'),'belongsTo'=>array('Customer','Category')));
+				$products = $this->Product->find('all',array('conditions'=>array('Product.customer_id'=>$customer_id),'order'=>'Product.name ASC'));
+										
+				//INJECT SALE DETAILS ON PRODUCTS ARRAY				
+				foreach($products as $k => $prdct){
+					//$products[$k]['sold'] = 0;
+					$products[$k]['returned'] = 0;
+					$products[$k]['delivered'] = 0;
+					$products[$k]['purchase_price'] = 0.00;	
+					$products[$k]['total_inventory'] = $prdct['Product']['beginning_inventory'];	
+					foreach($saleDetails as $sale_dtls){
+						if($prdct['Product']['id'] == $sale_dtls['products']['id']){
+							$products[$k]['Product'] = $prdct['Product'];
+							$products[$k]['returned'] = $sale_dtls[0]['total_returned'];
+							$products[$k]['delivered'] = $sale_dtls[0]['total_delivered'];
+							$products[$k]['purchase_price'] = $sale_dtls['delivery_details']['purchase_price'];
+							$products[$k]['total_inventory'] = ($prdct['Product']['beginning_inventory']+$sale_dtls[0]['total_delivered'])-$sale_dtls[0]['total_returned'];
+						}
+					}				
+				}
+				$data['Result'] = $products;
 		}else{
 			$data['is_posted'] = true;
 		}
-			
 		
+	
 		
 		echo json_encode($data);
 		exit;
