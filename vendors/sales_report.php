9c0c2e695a43abb4e2166864b388b173c0524109
Paulo <paulobiscocho>
2017-09-19 20:21:13 +0800

    Update & Adjust Sales Report

--- vendors/sales_report.php
+++ vendors/sales_report.php
@@ -39,8 +39,8 @@ class SalesReport extends Formsheet{
 		$this->leftText(4,$y,$data['Customer']['name'],'','');
 		$this->leftText(20,$y,'Date:','','b');
 		$this->leftText(22,$y++,date("M d", strtotime($data['Sale']['from_date'])).' - '.date("d, Y", strtotime($data['Sale']['to_date'])),'','');
-		$this->leftText(0,$y,'Posted:','','b');
-		$this->leftText(3,$y,($data['Sale']['is_posted'] == 1)?'True':'False','','');
+		//$this->leftText(0,$y,'Posted:','','b');
+		//$this->leftText(3,$y,($data['Sale']['is_posted'] == 1)?'True':'False','','');
 	}
 	
 	function dtls($data){
@@ -50,73 +50,84 @@ class SalesReport extends Formsheet{
 			'base_y'=> 1.8,
 			'width'=> 8,
 			'height'=> 8.8,
-			'cols'=> 40,
+			'cols'=> 43,
 			'rows'=> 44,	
 		);	
 		$this->section($metrics);
-		$this->GRID['font_size']=9;
+		$this->GRID['font_size']=8;
 		$this->drawBox(0,0,$metrics['cols'],$metrics['rows']);
 		$this->drawMultipleLines(3,43,1,'h');
 		$this->drawLine(2,'h');
-		$this->drawLine(1,'h',array(13,15));
-		$this->drawLine(13,'v',array(0,$metrics['rows']-1));
-		$this->drawLine(16,'v',array(1,$metrics['rows']-2));
-		$this->drawLine(19,'v',array(1,$metrics['rows']-2));
-		$this->drawLine(22,'v',array(0,$metrics['rows']-2));
-		$this->drawLine(25,'v',array(1,$metrics['rows']-2));
-		$this->drawLine(28,'v',array(0,$metrics['rows']-1));
+		$this->drawLine(9,'v',array(0,$metrics['rows']-1));
+		$this->drawLine(12,'v',array(0,$metrics['rows']-1));
+		$this->drawLine(15,'v',array(0,$metrics['rows']-1));
+		$this->drawLine(18,'v',array(0,$metrics['rows']-1));
+		$this->drawLine(21,'v',array(0,$metrics['rows']-1));
+		$this->drawLine(24,'v',array(0,$metrics['rows']-1));
+		$this->drawLine(27,'v',array(0,$metrics['rows']-1));
+		$this->drawLine(29,'v',array(0,$metrics['rows']-1));
 		$this->drawLine(31,'v',array(0,$metrics['rows']-1));
 		$this->drawLine(34,'v');
 		$this->drawLine(37,'v');
+		$this->drawLine(40,'v');
 		$y=1;
-		$this->centerText(0.2,1.2,'Item Description',16,'b');
-		
-		$this->centerText(13,0.8,'Quantity',9,'b');
-		$this->centerText(13,1.8,'Delivered',3,'');
-		$this->centerText(16,1.8,'Returned',3,'');
-		$this->centerText(19,1.8,'Sold',3,'');
+		$this->centerText(0,1.2,'Item Description',10,'b');
 		
 		
-		$this->centerText(22,1.8,'Inventory',3,'');
-		$this->centerText(25,1.8,'Oversold',3,'');
+		$this->centerText(9,0.9,'Beginning',3,'');
+		$this->centerText(9,1.8,'Inventory',3,'');
+		
+		$this->centerText(12,1.8,'Delivered',3,'');
+		$this->centerText(15,1.8,'Returned',3,'');
+		$this->centerText(18,1.8,'Sold',3,'');
 		
 		
-		$this->centerText(28,0.9,'Purchase',3,'b');
-		$this->centerText(28,1.7,'Price',3,'b');
+		$this->centerText(21,0.9,'Ending',3,'');
+		$this->centerText(21,1.8,'Inventory',3,'');
 		
-		$this->centerText(31,0.9,'Selling',3,'b');
-		$this->centerText(31,1.7,'Price',3,'b');
+		$this->centerText(24,0.9,'Actual',3,'');
+		$this->centerText(24,1.8,'Inventory',3,'');
 		
-		$this->centerText(34,1.2,'Cost',3,'b');
-		$this->centerText(37,1.2,'Profit',3,'b');
+		
+		$this->centerText(27,0.9,'Over',2,'');
+		$this->centerText(27,1.8,'Sold',2,'');
+		
+		$this->centerText(29,0.9,'Mis.',2,'');
+		$this->centerText(29,1.8,'Qty',2,'');
+		
+		
+		$this->centerText(31,0.9,'Purchase',3,'');
+		$this->centerText(31,1.7,'Price',3,'');
+		
+		$this->centerText(34,0.9,'Selling',3,'');
+		$this->centerText(34,1.7,'Price',3,'');
+		
+		$this->centerText(37,1.2,'Cost',3,'');
+		$this->centerText(40,1.2,'Profit',3,'');
 		$y=2.8;
 		$this->GRID['font_size']=8;
 		$total_cost = 0;
 		$total_profit = 0;
 		foreach($data['SaleDetail'] as $itm){
 			$this->leftText(0.2,$y,$itm['Product']['name'],'','');
-			$this->centerText(13,$y,$itm['delivered'],3,'');
-			$this->centerText(16,$y,$itm['returned'],3,'');
-			$this->centerText(19,$y,$itm['sold'],3,'');
-			if($itm['delivered'] > $itm['sold']){
-				$this->centerText(25,$y,'0',3,'');
-				$this->centerText(22,$y,$itm['delivered']-$itm['sold'],3,'');
-			}else if($itm['delivered'] < $itm['sold']){
-				$this->centerText(25,$y,$itm['sold'] - $itm['delivered'],3,'');
-				$this->centerText(22,$y,'0',3,'');
-			}else{
-				$this->centerText(25,$y,'0',3,'');
-				$this->centerText(22,$y,'0',3,'');
-			}
+			$this->centerText(9,$y,$itm['beginning_inventory'],3,'');
+			$this->centerText(12,$y,$itm['delivered'],3,'');
+			$this->centerText(15,$y,$itm['returned'],3,'');
+			$this->centerText(18,$y,$itm['sold'],3,'');
+			$this->centerText(21,$y,$itm['ending_inventory'],3,'');
+			$this->centerText(24,$y,$itm['Product']['beginning_inventory'],3,'');
+			$this->centerText(27,$y,$itm['over_sold'],2,'');
+			$this->centerText(29,$y,$itm['missing_qty'],2,'');
+	
 			
 			$cost = $itm['Product']['purchase_price']*$itm['sold'];
 			$profit = ($itm['Product']['selling_price']*$itm['sold'])-$cost;
 			$total_cost+=$cost;
 			$total_profit+=$profit;
-			$this->rightText(27.9,$y,$itm['Product']['purchase_price'],3,'');
-			$this->rightText(30.9,$y,$itm['Product']['selling_price'],3,'');
-			$this->rightText(36.9,$y,number_format($cost,2),'','');
-			$this->rightText(39.9,$y,number_format($profit,2),'','');
+			$this->rightText(30.9,$y,$itm['Product']['purchase_price'],3,'');
+			$this->rightText(33.9,$y,$itm['Product']['selling_price'],3,'');
+			$this->rightText(39.9,$y,number_format($cost,2),'','');
+			$this->rightText(42.9,$y,number_format($profit,2),'','');
 			
 			$y++;
 		}
@@ -126,8 +137,8 @@ class SalesReport extends Formsheet{
 		$y = 43.8;
 		$this->GRID['font_size']=8;
 		$this->leftText(0.2,$y,'TOTAL','','b');
-		$this->rightText(36.9,$y,number_format($total_cost,2),'','b');
-		$this->rightText(39.9,$y,number_format($total_profit,2),'','b');
+		$this->rightText(39.9,$y,number_format($total_cost,2),'','b');
+		$this->rightText(42.9,$y,number_format($total_profit,2),'','b');
 	}
 
 }
