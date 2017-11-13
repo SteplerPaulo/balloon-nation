31558e3c5de91434890cff9c7cf533fa5e11aad0
Paulo <paulobiscocho>
2017-11-13 18:23:49 +0800

    Admin Posting Prevent Double Clink On Save

--- webroot/js/controllers/admin_posting.js
+++ webroot/js/controllers/admin_posting.js
@@ -2,6 +2,7 @@ App.controller('AdminSalePostingController',function($scope,$rootScope,$http,$fi
 	$scope.initializeController = function(){
 		$scope.currentPage = 1; 
 		$scope.pageSize = 35;
+		$scope.preventDoubleClick = false;
 		
 		$scope.SaleID =  $("#SalePostingPanel").attr('sale-id');
 		$http.get(BASE_URL+"sales/posting_data/"+$scope.SaleID).success(function(response) {
@@ -17,26 +18,26 @@ App.controller('AdminSalePostingController',function($scope,$rootScope,$http,$fi
 	}
 
 	$scope.post = function (){
+		$scope.preventDoubleClick = true;
 	
 		var data = {'Sale':{
 						'id':$scope.data.Sale.id,
 						'is_posted':1,
 						'SaleDetail':$scope.data.SaleDetail,
 					}};
-			data['Product'] = [];
+			data['Product'] = {};
 			
 		for (var i = 0; i < $scope.data.SaleDetail.length; i++) {
+			//console.log($scope.data.SaleDetail[i]);
 			data['Product'][i] = {
 							'id':$scope.data.SaleDetail[i].product_id,
 							'beginning_inventory':$scope.data.SaleDetail[i].actual_inventory,
 							'missing_qty':$scope.data.SaleDetail[i].missing_qty,
 						};
-						
 		}
 		
-			
-		console.log(data);
-		//return;
+		//console.log(data);
+		
 		$http({
 			method: 'POST',
 			url: BASE_URL+'sales/posting_saving',
@@ -48,7 +49,6 @@ App.controller('AdminSalePostingController',function($scope,$rootScope,$http,$fi
 		
 		
 	}
-	
 	
 });
 
