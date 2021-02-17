228783b02afbe34472cb3b1bbcf994a601fe9c76
SteplerPaulo <paulobiscocho@gmail.com>
2021-02-17 15:52:25 +0800

    Updates for Online

--- webroot/js/constant.js
+++ webroot/js/constant.js
@@ -1,5 +1,5 @@
-if (document.location.hostname == "localhost"){
+if (window.location.origin == "localhost"){
 	var BASE_URL = '/balloon-nation/';
 }else{
-	var	BASE_URL =  window.location.origin;
+	var	BASE_URL =  window.location.origin+'/';
 }
