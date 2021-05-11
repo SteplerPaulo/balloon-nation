3c3538bc05a920df29c1df13f6acb1f56e242e4a
SteplerPaulo <paulobiscocho@gmail.com>
2021-05-11 16:43:51 +0800

    Update Admin Dashboard.js

--- webroot/js/controllers/admin_dashboard.js
+++ webroot/js/controllers/admin_dashboard.js
@@ -1,6 +1,12 @@
 App.controller('DashboardController',function($scope,$rootScope,$http,$filter,$uibModal, $log, $document){
 	$scope.initializeController = function(){
 		
+		$http.get(BASE_URL+"/years/all").success(function(response) {
+			$scope.years = response;
+			
+			
+		});
+		
 		$http.get(BASE_URL+"/users/dashboard_updates").success(function(response) {
 			$scope.forDelivery = response.ForDelivery;
 			$scope.forPosting = response.ForPosting;
