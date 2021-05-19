dd74511fa28ac771fa833bc31f418450662789d4
SteplerPaulo <paulobiscocho@gmail.com>
2021-05-19 13:21:02 +0800

    Update Admin Sales JS

--- webroot/js/controllers/admin_sales.js
+++ webroot/js/controllers/admin_sales.js
@@ -4,7 +4,7 @@ App.controller('AdminSalesController',function($scope,$rootScope,$http,$filter,$
 		$scope.customer_id = window.location.search.split('&')[0].substring(1);
 		$scope.customer_name = decodeURI(window.location.search.split('&')[1]);
 			
-		$http.get(BASE_URL+"sales/customer_sales/"+$scope.customer_id).success(function(response) {
+		$http.get(BASE_URL+"customers/sales/"+$scope.customer_id).success(function(response) {
 			$scope.sales = response;
 			$scope.loading = false;
 		});
