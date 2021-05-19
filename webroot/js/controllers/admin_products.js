cdd473ad6bfac4872c54af55269e49e987766de3
SteplerPaulo <paulobiscocho@gmail.com>
2021-05-19 13:00:32 +0800

    Update Admin Products- Filter By Customer ID

--- webroot/js/controllers/admin_products.js
+++ webroot/js/controllers/admin_products.js
@@ -4,34 +4,35 @@ App.controller('AdminProductsController',function($scope,$rootScope,$http,$filte
 		$scope.pageSize = 17;
 		$scope.loading = true;
 			
-		/*DONT KNOW WHAT THE USE OF THIS CODE
-		if (document.location.hostname == "localhost"){if(window.location.pathname.split('/')[5]){$rootScope.customer = decodeURI(window.location.pathname.split('/')[5]);}
-		}else{
-			if(window.location.pathname.split('/')[2]){$rootScope.customer = decodeURI(window.location.pathname.split('/')[4]);	}
-		}*/
+		$scope.customer_id = window.location.search.split('&')[0].substring(1);
+		$scope.customer_name = decodeURI(window.location.search.split('&')[1]);
 		
-		
-		$http.get(BASE_URL+"/products/customers").success(function(response) {
+		/* $http.get(BASE_URL+"/products/customers").success(function(response) {
 			$rootScope.customers = response;
 			if($rootScope.customer ==  undefined || $rootScope.customer ==  'undefined'){
 				$rootScope.customer = 'Balloon Nation';
 			}
-		});
+		}); */
 		
 		
-		$http.get(BASE_URL+"/admin/products/init").success(function(response) {
+		$http.get(BASE_URL+"customers/products/"+$scope.customer_id).success(function(response) {
 			$scope.products = response;
 			$scope.loading = false;
 		});
+		
+		/* $http.get(BASE_URL+"/admin/products/init").success(function(response) {
+			$scope.products = response;
+			$scope.loading = false;
+		}); */
 
 	}
 	
-	$scope.changeCustomer = function(){
+	/* $scope.changeCustomer = function(){
 		$scope.loading = true;
 		$http.get(BASE_URL+"/admin/products/init/"+$scope.customer).success(function(response) {
 			$scope.products = response;
 			$scope.loading = false;
 		});
-	} 
+	}  */
 });
 
