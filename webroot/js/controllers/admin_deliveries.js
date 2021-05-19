1b80f2d2f4ddda52d2c5f0540f0f919869bb5b4b
SteplerPaulo <paulobiscocho@gmail.com>
2021-05-19 13:00:21 +0800

    Update Admin Deliveries  - Filter By Customer ID

--- webroot/js/controllers/admin_deliveries.js
+++ webroot/js/controllers/admin_deliveries.js
@@ -1,19 +1,16 @@
 App.controller('AdminDeliveriesController',function($scope,$rootScope,$http,$filter,$uibModal, $log, $document){
 	$rootScope.initializeController = function(){
 		$scope.currentPage = 1; 
-		$scope.pageSize = 12;
+		$scope.pageSize = 8;
 		$scope.loading = true;
+		$scope.customer_id = window.location.search.split('&')[0].substring(1);
+		$scope.customer_name = decodeURI(window.location.search.split('&')[1]);
 		
-		$http.get(BASE_URL+"deliveries/customers").success(function(response) {
-			$rootScope.customers = response;
-			if($rootScope.customer ==  undefined){
-				$rootScope.customer = '';
-			}
-		});
 		
-		$http.get(BASE_URL+"deliveries/all").success(function(response) {
+		$http.get(BASE_URL+"customers/deliveries/"+$scope.customer_id).success(function(response) {
 			$scope.deliveries = response;
 			$scope.loading = false;
-		});		
+		});
+		
 	}
 });
