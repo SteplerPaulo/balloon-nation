7e6e7522c46d63f139b2455533633d4bb685cab6
SteplerPaulo <paulobiscocho@gmail.com>
2021-05-19 13:21:21 +0800

    Update Admin Product JS

--- webroot/js/controllers/admin_products.js
+++ webroot/js/controllers/admin_products.js
@@ -7,32 +7,13 @@ App.controller('AdminProductsController',function($scope,$rootScope,$http,$filte
 		$scope.customer_id = window.location.search.split('&')[0].substring(1);
 		$scope.customer_name = decodeURI(window.location.search.split('&')[1]);
 		
-		/* $http.get(BASE_URL+"/products/customers").success(function(response) {
-			$rootScope.customers = response;
-			if($rootScope.customer ==  undefined || $rootScope.customer ==  'undefined'){
-				$rootScope.customer = 'Balloon Nation';
-			}
-		}); */
-		
-		
+
 		$http.get(BASE_URL+"customers/products/"+$scope.customer_id).success(function(response) {
 			$scope.products = response;
 			$scope.loading = false;
 		});
 		
-		/* $http.get(BASE_URL+"/admin/products/init").success(function(response) {
-			$scope.products = response;
-			$scope.loading = false;
-		}); */
-
 	}
 	
-	/* $scope.changeCustomer = function(){
-		$scope.loading = true;
-		$http.get(BASE_URL+"/admin/products/init/"+$scope.customer).success(function(response) {
-			$scope.products = response;
-			$scope.loading = false;
-		});
-	}  */
 });
 
