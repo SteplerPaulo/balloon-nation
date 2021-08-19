4f637afcc2ac785206b8a67fa05ac1c46f2b7e2c
SteplerPaulo <paulobiscocho@gmail.com>
2021-08-19 19:26:57 +0800

    Update Delivery - Dashboard

--- models/delivery.php
+++ models/delivery.php
@@ -33,5 +33,39 @@ class Delivery extends AppModel {
 			'counterQuery' => ''
 		)
 	);
+	
+	
+	public function dashboard($year){
+		return $this->query(
+			"SELECT 
+			  products.id,
+			  products.name,
+			  customers.name,
+			  SUM(delivery_details.deliver) AS total_qty_delivered,
+			  delivery_details.purchase_price,
+			  delivery_details.selling_price,
+			  COUNT(*) AS no_of_times_delivered
+			FROM
+			  `deliveries` 
+			  INNER JOIN `delivery_details` 
+				ON (
+				  `deliveries`.`id` = `delivery_details`.`delivery_id`
+				) 
+			  INNER JOIN `customers` 
+				ON (
+				  `deliveries`.`customer_id` = `customers`.`id`
+				) 
+			  INNER JOIN `products` 
+				ON (
+				  `delivery_details`.`product_id` = `products`.`id`
+				) 
+			WHERE deliveries.created LIKE '%$year%' 
+			GROUP BY products.name,
+			  delivery_details.purchase_price,
+			  delivery_details.selling_price 
+			ORDER BY products.name 
+			"
+		);
+	}
 
 }
